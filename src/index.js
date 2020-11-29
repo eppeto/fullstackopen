e47dd7cbcf12782eeec420cdf8b3ae1d2e797eb1
eppeto <eppu.savolainen@edu.savonia.fi>
2020-11-29 14:55:02 +0200

    commit on part1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Hello = (props) => {
+  return (
+    <div>
+        <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+  const nimi = 'Pekka'
+  const ika = 10
+  return (
+  <div>
+    <h1>Greetings</h1>
+    <Hello name="Maya" age={26 + 10} />
+    <Hello name={nimi} age={ika} />
+    <Footer />
+  
+  </div>
+)
+  }
+
+  const Footer = () => {
+    return (
+      <div>
+        greeting app created by 
+        <a href="https://github.com/eppeto">Eppu Savolainen</a>
+      </div>
+    )
+  }
+
+ReactDOM.render(<App />, document.getElementById('root'))
